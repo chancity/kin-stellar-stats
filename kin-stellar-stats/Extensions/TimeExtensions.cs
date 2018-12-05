ac914e3021d9e09880f1f1c15e3ef955b50b462b
chancity <hawbakerchancey@gmail.com>
2018-12-04 18:01:09 -0800

    switched to unix time on stats dictionary

---
+++ kin-stellar-stats/Extensions/TimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kin.Horizon.Api.Poller.Extensions
+{
+    internal static class TimeExtensions
+    {
+        public static DateTime FromUnixTime(this long unixTime)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddSeconds(unixTime);
+        }
+
+        public static long ToEpochNearestHour(this DateTimeOffset date)
+        {
+            long roundTicks = TimeSpan.TicksPerHour;
+            return new DateTimeOffset(new DateTime(date.Ticks - date.Ticks % roundTicks)).ToUnixTimeSeconds();
+        }
+
+    }
+}
--- kin-stellar-stats/Services/Impl/StatsManager.cs
+++ kin-stellar-stats/Services/Impl/StatsManager.cs
@@ -4,36 +4,40 @@ using System.Collections.Generic;
 using System.Threading;
 using discord_web_hook_logger;
 using Kin.Horizon.Api.Poller.Database.StellarObjectWrappers;
+using Kin.Horizon.Api.Poller.Extensions;
 using Microsoft.Extensions.Logging;
 
 namespace Kin.Horizon.Api.Poller.Services.Impl
 {
     internal class StatsManager
     {
-        public DateTime? CurrentDate => _currentDate;
+        public DateTime CurrentDate => _currentDate.FromUnixTime();
 
-        public ConcurrentDictionary<DateTime, DailyStats> DailyStats { get; }
+        public ConcurrentDictionary<long, DailyStats> DailyStats { get; }
         private readonly IDiscordLogger _logger;
-        private DateTime? _currentDate;
+        private long _currentDate = long.MinValue;
 
         public StatsManager()
         {
-            DailyStats = new ConcurrentDictionary<DateTime, DailyStats>();
+            DailyStats = new ConcurrentDictionary<long, DailyStats>();
             _logger = DicordLogFactory.GetLogger<StatsManager>(GlobalVariables.DiscordId, GlobalVariables.DiscordToken);
         }
 
         public void HandleOperation(FlattenedOperation operation)
         {
-            if (CurrentDate == null || operation.CreatedAt > _currentDate)
-                _currentDate = operation.CreatedAt.Date;
+           
+            var epochTime = operation.CreatedAt.ToEpochNearestHour();
 
-            var dailyStats = DailyStats.GetOrAdd(operation.CreatedAt.Date, new DailyStats(operation.CreatedAt.Date));
+            if (epochTime > _currentDate)
+                _currentDate = epochTime;
+
+            var dailyStats = DailyStats.GetOrAdd(epochTime, new DailyStats(epochTime));
             dailyStats.HandleOperation(operation);
         }
 
         public void OutPutToDiscord()
         {
-            if (_currentDate != null && DailyStats.TryGetValue(_currentDate.Value, out var today))
+            if (DailyStats.TryGetValue(_currentDate, out var today))
             {
                 _logger.LogInformation(today.ToString());
                 foreach (KeyValuePair<string, AppStats> todayAppStat in today.AppStats)
@@ -47,14 +51,14 @@ namespace Kin.Horizon.Api.Poller.Services.Impl
 
     internal class DailyStats : Stats
     {
-        public DateTime Date { get; set; }
+        public long EpochTime { get; set; }
         public HashSet<string> ActiveWallets { get; }
         public ConcurrentDictionary<string, AppStats> AppStats { get; }
         private readonly object _activeUsersLockObject;
 
-        public DailyStats(DateTime date)
+        public DailyStats(long epochTime)
         {
-            Date = date;
+            EpochTime = epochTime;
             _activeUsersLockObject = new object();
             ActiveWallets = new HashSet<string>();
             AppStats = new ConcurrentDictionary<string, AppStats>();
@@ -96,7 +100,7 @@ namespace Kin.Horizon.Api.Poller.Services.Impl
         public override string ToString()
         {
             return $"\nAggregated Stats\n" +
-                   $"Date: {Date}\n" +
+                   $"Date: {EpochTime.FromUnixTime()}\n" +
                    $"{base.ToString()}";
         }
     }
